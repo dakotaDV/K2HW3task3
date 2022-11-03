c4ad005ce43f7e65b7838bd019f417f71a6e032c
Prudnikova <dakoto4ka@yandex.ru>
2022-11-03 16:06:48 +0300

    дз2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import Transport.Car;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+            Car lada = new Car("Lada", "Granta", 1.7,"желтый",2015,  "в России", "АКПП", "хэтчбэк", "а555тл333", 6,   false);
+            Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный ", 2020, "в Германии", "АКПП", "седан", "л478гн963", 6,  true);
+            Car bmw = new Car("BMW", "Z8", 3.0, "черный", 2021, "в Германии", "МКПП", "хэтчбэк", "к777нг444", 5,  true);
+            Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "в Южной Корее", "МКПП", "седан", "а333чс5555", 6,  false);
+            Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "в Южной Корее", "АКПП", "хэтчбэк", "и223до789", 4,  true);
+
+
+
+        }
+
     }
-}
+
+
+
